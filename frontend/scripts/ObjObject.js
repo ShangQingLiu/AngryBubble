76859c0aa604840f896c06bbe729eb0d681ca95a
luoyeltlt <luoyeltlt@gmail.com>
2018-01-11 19:26:21 +0800

    update

--- frontend/scripts/ObjObject.js
+++ frontend/scripts/ObjObject.js
@@ -5,87 +5,111 @@ const NSLINE = 12;
 
 $.ajaxSettings.ajax = false;
 
-function ObjObject(filepath, _gl, _shaderProgram){
-     this.basicElements = [];
-    // this.program = _shaderProgram;
-    // this.gl = _gl;
-    // this.vsModelMatrix = _gl.getUniformLocation(this.program, 'vsModelMatrix');
-    // this.vsMvpMatrix = _gl.getUniformLocation(this.program, 'vsMvpMatrix');
-    // this.vsNormalMatrix = _gl.getUniformLocation(this.program, 'vsNormalMatrix');
-    //
-    // this.vsKa = _gl.getUniformLocation(this.program, 'vsKa');
-    // this.vsKd = _gl.getUniformLocation(this.program, 'vsKd');
-    // this.vsKs = _gl.getUniformLocation(this.program, 'vsKs');
-    //
-    // this.vsPosition = _gl.getAttribLocation(this.program, 'vsPosition');
-    // this.vsNormal  = _gl.getAttribLocation(this.program, 'vsNormal');
-    //
+
+
+function ObjObject(filePath, _gl, _shaderProgram){
+    this.basicElements = [];
+    this.program = _shaderProgram;
+    this.gl = _gl;
+    this.uModelMatrix = _gl.getUniformLocation(this.program, 'uModelMatrix');
+    this.uViewMatrix = _gl.getUniformLocation(this.program, 'uViewMatrix');
+    this.uProjectionMatrix = _gl.getUniformLocation(this.program, 'uProjectionMatrix');
+    this.uNormalMatrix = _gl.getUniformLocation(this.program, 'uNormalMatrix');
+    this.uSampler = _gl.getUniformLocation(this.program, 'uSampler');
+
+    this.aPosition = _gl.getAttribLocation(this.program, 'aPosition');
+    this.aNormal  = _gl.getAttribLocation(this.program, 'aNormal');
+    this.aTextureCoord  = _gl.getAttribLocation(this.program, 'aTextureCoord');
+    this.uKa = _gl.getUniformLocation(this.program, 'uKa');
+    this.uKd = _gl.getUniformLocation(this.program, 'uKd');
+    this.uKs = _gl.getUniformLocation(this.program, 'uKs');
+    this.uNs = _gl.getUniformLocation(this.program, 'uNs');
+
 
     this.modelMatrix = new Matrix4();
     this.modelMatrix.setRotate(270, 1, 0, 0);
 
+    this.draw = function (viewMatrix, projectionMatrix) {
+        var normalMatrix = new Matrix4();
+        var gl = this.gl;
 
-    // this.draw = function (eyeMatrix) {
-    //     var mvpMatrix = new Matrix4(eyeMatrix);
-    //     var normalMatrix = new Matrix4();
-    //     var gl = this.gl;
-    //
-    //     mvpMatrix.multiply(this.modelMatrix);
-    //     normalMatrix.setInverseOf(this.modelMatrix);
-    //     normalMatrix.transpose();
-    //
-    //     gl.uniformMatrix4fv(this.vsModelMatrix, false, this.modelMatrix.elements);
-    //     gl.uniformMatrix4fv(this.vsMvpMatrix, false, mvpMatrix.elements);
-    //     gl.uniformMatrix4fv(this.vsNormalMatrix, false, normalMatrix.elements);
-    //
-    //
-    //     for(var i = 0; i != this.basicElements.length; ++i){
-    //         console.log("start draw element")
-    //         console.log()
-    //
-    //
-    //
-    //
-    //
-    //         var basicElement = this.basicElements[i];
-    //         var mtl = basicElement.mtl;
-    //
-    //
-    //         gl.uniform3f(this.vsKa, mtl.Ka[0], mtl.Ka[1], mtl.Ka[2]);
-    //         gl.uniform3f(this.vsKd, mtl.Kd[0], mtl.Kd[1], mtl.Kd[2]);
-    //         gl.uniform3f(this.vsKs, mtl.Ks[0], mtl.Ks[1], mtl.Ks[2]);
-    //
-    //
-    //         gl.bindBuffer(gl.ARRAY_BUFFER, basicElement.positionBufferHandle);
-    //         gl.bufferData(gl.ARRAY_BUFFER, basicElement.positionData, gl.STATIC_DRAW);
-    //         gl.vertexAttribPointer(this.vsPosition, 3, gl.FLOAT, false, 0, 0);
-    //         gl.enableVertexAttribArray(this.vsPosition);  // Enable the assignment of the buffer object
-    //
-    //
-    //         gl.bindBuffer(gl.ARRAY_BUFFER, basicElement.normalBufferHandle);
-    //         gl.bufferData(gl.ARRAY_BUFFER, basicElement.normalData, gl.STATIC_DRAW);
-    //         gl.vertexAttribPointer(this.vsNormal, 3, gl.FLOAT, false, 0, 0);
-    //         gl.enableVertexAttribArray(this.vsNormal);  // Enable the assignment of the buffer object
-    //
-    //
-    //         gl.drawArrays(gl.TRIANGLES, 0, basicElement.vertexNum);
-    //
-    //         gl.bindBuffer(gl.ARRAY_BUFFER, null);
-    //
-    //     }
-    //
-    //
-    // };
-    //
+
+        gl.useProgram(this.program);
+        normalMatrix.setInverseOf(this.modelMatrix);
+        normalMatrix.transpose();
+
+        gl.uniformMatrix4fv(this.uModelMatrix, false, this.modelMatrix.elements);
+        gl.uniformMatrix4fv(this.uViewMatrix, false, viewMatrix.elements);
+        gl.uniformMatrix4fv(this.uProjectionMatrix, false, projectionMatrix.elements);
+        gl.uniformMatrix4fv(this.uNormalMatrix, false, normalMatrix.elements);
+
+
+
+        for(var i = 0; i != this.basicElements.length; ++i){
+
+
+
+            var basicElement = this.basicElements[i];
+            var mtl = basicElement.mtl;
+
+            if(!basicElement.ready.imageReady){
+                console.log("data not prepared")
+                return;
+            }
+
+
+            gl.uniform3f(this.uKa, mtl.Ka[0], mtl.Ka[1], mtl.Ka[2]);
+            gl.uniform3f(this.uKd, mtl.Kd[0], mtl.Kd[1], mtl.Kd[2]);
+            gl.uniform3f(this.uKs, mtl.Ks[0], mtl.Ks[1], mtl.Ks[2]);
+            gl.uniform1f(this.uNs, mtl.Ns);
+
+
+
+            gl.bindBuffer(gl.ARRAY_BUFFER, basicElement.positionBufferHandle);
+            gl.bufferData(gl.ARRAY_BUFFER, basicElement.positionData, gl.STATIC_DRAW);
+            gl.vertexAttribPointer(this.aPosition, 3, gl.FLOAT, false, 0, 0);
+            gl.enableVertexAttribArray(this.aPosition);  // Enable the assignment of the buffer object
+
+            gl.bindBuffer(gl.ARRAY_BUFFER, basicElement.coordBufferHandle);
+            gl.bufferData(gl.ARRAY_BUFFER, basicElement.textureCoordData, gl.STATIC_DRAW);
+            gl.vertexAttribPointer(this.aTextureCoord, 2, gl.FLOAT, false, 0, 0);
+            gl.enableVertexAttribArray(this.aTextureCoord);  // Enable the assignment of the buffer object
+
+
+            gl.bindBuffer(gl.ARRAY_BUFFER, basicElement.normalBufferHandle);
+            gl.bufferData(gl.ARRAY_BUFFER, basicElement.normalData, gl.STATIC_DRAW);
+            gl.vertexAttribPointer(this.aNormal, 3, gl.FLOAT, false, 0, 0);
+            gl.enableVertexAttribArray(this.aNormal);  // Enable the assignment of the buffer object
+
+
+            gl.uniform1i(this.uSampler, 0)
+
+            gl.activeTexture(gl.TEXTURE0)
+            gl.bindTexture(gl.TEXTURE_2D, basicElement.textureHandle);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST)
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
+
+
+            gl.drawArrays(gl.TRIANGLES, 0, basicElement.vertexNum);
+
+            gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+        }
+
+
+    };
+
 
     var objStr;
-    var tempDir = filepath.split('/');
+    var tempDir = filePath.split('/');
     var dirPath = "";
     for(var i = 0; i != tempDir.length - 1; ++i){
         dirPath += tempDir[i] + '/';
     }
 
-    $.get(filepath, function (content) {
+    $.get(filePath, function (content) {
         objStr = content;
     });
     var lines = objStr.split('\n');
@@ -143,11 +167,18 @@ function ObjObject(filepath, _gl, _shaderProgram){
                     var position0 = positionBuffer[indexVector[0] - 1];
                     var position1 = positionBuffer[indexVector[2] - 1];
                     var position2 = positionBuffer[indexVector[4] - 1];
-
+                    var position4 = positionBuffer[indexVector[6] - 1];
 
                     var normalVector = getNormal(position0, position1, position2);
                     for(var i = 0; i != 3; ++i){
-                        var positionVector = positionBuffer[indexVector[i] - 1];
+                        var positionVector = positionBuffer[indexVector[i*2] - 1];
+                        for(var j = 0; j != 3; ++j){
+                            currentElement.vertexPositions.push(positionVector[j]);
+                            currentElement.vertexNormals.push(normalVector[j]);
+                        }
+                    }
+                    for(var i = 1; i != 4; ++i){
+                        var positionVector = positionBuffer[indexVector[i*2] - 1];
                         for(var j = 0; j != 3; ++j){
                             currentElement.vertexPositions.push(positionVector[j]);
                             currentElement.vertexNormals.push(normalVector[j]);
@@ -155,6 +186,12 @@ function ObjObject(filepath, _gl, _shaderProgram){
                     }
 
                     for (var i = 0; i != 3; ++i){
+                        var coords = coordsBuffer[indexVector[i*2 + 1] - 1];
+                        for(var j = 0; j != 2; ++j){
+                            currentElement.textureCoords.push(coords[j]);
+                        }
+                    }
+                    for (var i = 1; i != 4; ++i){
                         var coords = coordsBuffer[indexVector[i*2 + 1] - 1];
                         for(var j = 0; j != 2; ++j){
                             currentElement.textureCoords.push(coords[j]);
@@ -193,6 +230,7 @@ function ObjObject(filepath, _gl, _shaderProgram){
 
     }
     this.basicElements.push(currentElement);
+    console.log(this)
     for(var i = 0; i != this.basicElements.length; ++i){
         this.basicElements[i].prepareData();
     }
@@ -275,6 +313,9 @@ function getType(line) {
         case 'Ks':
             type = KSLINE;
             break;
+        case 'Ns':
+            type = NSLINE;
+            break;
         case 'map_Kd':
             type = MAP_KD_LINE;
             break;
@@ -327,7 +368,7 @@ function getMtls(line, dirPath){
                 currentMtl.Ns = parseFloat(getSecondStr(line));
                 continue;
             case MAP_KD_LINE:
-                currentMtl.map_Kd = getSecondStr(line);
+                currentMtl.map_Kd = dirPath + getSecondStr(line);
                 continue;
         }
     }
@@ -346,14 +387,41 @@ function BasicElement(gl) {
     this.vertexNum = 0;
     this.positionBufferHandle = null;
     this.normalBufferHandle = null;
-    //this.positionBufferHandle = gl.createBuffer();
-    //this.normalBufferHandle = gl.createBuffer();
+    this.coordBufferHandle = null;
+    this.textureHandle = gl.createTexture();
+    this.positionBufferHandle = gl.createBuffer();
+    this.normalBufferHandle = gl.createBuffer();
+    this.coordBufferHandle = gl.createBuffer();
+    this.image = new Image();
 
     this.prepareData = function () {
         this.positionData = new Float32Array(this.vertexPositions);
         this.normalData = new Float32Array(this.vertexNormals);
         this.textureCoordData = new Float32Array(this.textureCoords);
         this.vertexNum = this.vertexPositions.length / 3;
+        this.image.src = this.mtl.map_Kd;
+        let image = this.image;
+        let textureHandle = this.textureHandle;
+        this.ready = {};
+        this.ready.imageReady = false;
+        let ready = this.ready;
+        this.image.onload = function () {
+
+
+            gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1); // Flip the image's y axis
+            // Enable texture unit0;
+            gl.activeTexture(gl.TEXTURE0);
+            // Bind the texture object to the target
+            gl.bindTexture(gl.TEXTURE_2D, textureHandle)
+
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+            gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, image);
+            ready.imageReady = true;
+
+        };
     }
 }
 
@@ -361,7 +429,7 @@ function MTLInfo() {
     this.Ka = [0.0, 0.0, 0.0];
     this.Ks = [0.0, 0.0, 0.0];
     this.Kd = [1.0, 1.0, 1.0];
-    this.Ns = 1.0;
+    this.Ns = 1000.0;
     this.map_Kd = "";
 }
 
